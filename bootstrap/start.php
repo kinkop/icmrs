27dd51bbbc467cd55a450507c913847f40be4897
Pakin Mnakong <pakinmankong@gmail.com>
2014-12-30 15:41:56 +0700

    Edit

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead'),
-    'live' => array('ssru.ac.th')
+    'live' => array('ssru')
 
 ));
 
